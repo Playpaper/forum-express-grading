46546d6cd122f928d84c0f8b76666517556ddf8c
Playpaper <heartcolorful@gmail.com>
2023-05-30 13:48:18 +0800

    feat: show admin restaurants from db

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,12 @@
+const { Restaurant } = require('../models')
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: async (req, res, next) => {
+    try {
+      const restaurants = await Restaurant.findAll({ raw: true, nest: true })
+      return res.render('admin/restaurants', { restaurants })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
