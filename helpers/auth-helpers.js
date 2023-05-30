84951637ed06262c14bbca19084ce0e8b34e3e55
Playpaper <heartcolorful@gmail.com>
2023-05-30 12:24:06 +0800

    feat: user model add is_admin

--- helpers/auth-helpers.js
+++ helpers/auth-helpers.js
@@ -1,12 +1,19 @@
 module.exports = {
   getUser: req => {
     return req.user || null
+  },
+  ensureAuthenticated: req => {
+    return req.isAuthenticated()
   }
 }
 
 // const getUser = req => {
 //   return req.user || null
 // }
+// const ensureAuthenticated = req => {
+//   return req.isAuthenticated()
+// }
 // module.exports = {
-//   getUser
+//   getUser,
+//   ensureAuthenticated
 // }
